c0b4bfeac2587694e5d7fe623635bb779f5d725e
JJvdM83 <jacques.vandermerwe2@gmail.com>
2025-01-11 18:49:37 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,21 @@
 let Sonor = 0
-music.setVolume(255)
 music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
-basic.showString("Hello JJ")
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.showString("3...2...1...")
 wuKong.setAllMotor(-20, -30)
 basic.forever(function () {
     Sonor = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
-    if (Sonor < 20 && Sonor > 1) {
+    while (Sonor < 20 && Sonor > 1) {
         if (Math.randomBoolean()) {
             wuKong.setAllMotor(-50, -10)
             basic.pause(randint(10, 200))
             music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
-            basic.showLeds(`
-                . # . . .
-                . # . . .
-                . # . . .
-                . # . . .
-                . # # # .
-                `)
         } else {
             wuKong.setAllMotor(-10, -50)
             basic.pause(randint(10, 200))
             music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
-            basic.showLeds(`
-                . # # # .
-                . # . # .
-                . # # . .
-                . # . # .
-                . # . # .
-                `)
         }
-    } else {
-        wuKong.setAllMotor(-20, -30)
     }
+    wuKong.setAllMotor(-20, -30)
 })
