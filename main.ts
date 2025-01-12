9ff2f38431caae8bb37b3b7f731ae0474417baf7
JJvdM83 <jacques.vandermerwe2@gmail.com>
2025-01-12 12:56:00 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.AB, function () {
+    wuKong.setAllMotor(-20, -30)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    wuKong.stopAllMotor()
+})
 let Sonor = 0
 music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
 basic.showIcon(IconNames.Happy)
@@ -9,12 +15,12 @@ basic.forever(function () {
     Sonor = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
     while (Sonor < 20 && Sonor > 1) {
         if (Math.randomBoolean()) {
-            wuKong.setAllMotor(-50, 10)
-            basic.pause(randint(10, 200))
+            wuKong.setAllMotor(50, 0)
+            basic.pause(randint(100, 500))
             music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
         } else {
-            wuKong.setAllMotor(10, -50)
-            basic.pause(randint(10, 200))
+            wuKong.setAllMotor(0, 50)
+            basic.pause(randint(200, 500))
             music.play(music.tonePlayable(494, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
         }
         Sonor = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
