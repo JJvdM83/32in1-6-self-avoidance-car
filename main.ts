7c4dd4293a3ee55be5c4c5f60fc2591f3151f564
JJvdM83 <jacques.vandermerwe2@gmail.com>
2025-01-12 12:49:29 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,12 +10,14 @@ basic.forever(function () {
     while (Sonor < 20 && Sonor > 1) {
         if (Math.randomBoolean()) {
             wuKong.setAllMotor(-50, 10)
+            basic.pause(randint(10, 200))
+            music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
         } else {
             wuKong.setAllMotor(10, -50)
+            basic.pause(randint(10, 200))
+            music.play(music.tonePlayable(494, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
         }
-        basic.pause(randint(10, 200))
         Sonor = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
+        wuKong.setAllMotor(-20, -30)
     }
-    music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.InBackground)
-    wuKong.setAllMotor(-20, -30)
 })
